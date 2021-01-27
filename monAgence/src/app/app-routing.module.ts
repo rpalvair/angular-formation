e3295bf1a85ff129e2179091b8cca32331fd19b3
ruddy palvair <r.palvair@gmail.com>
2021-01-27 11:33:39 +0100

    feat : Add module for routing

---
+++ monAgence/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+
+const routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
--- monAgence/src/app/app.module.ts
+++ monAgence/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -14,7 +15,8 @@ import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard
     AdminDashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
