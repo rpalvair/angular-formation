85dbebaa01aafb80d45855aa8996aec2eff2b28c
ruddy palvair <r.palvair@gmail.com>
2021-01-27 09:12:33 +0100

    feat/bindings (#2)

--- monAgence/src/app/header/header.component.ts
+++ monAgence/src/app/header/header.component.ts
@@ -8,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   navbarTitle = "AgenceImmo"
+  private disabled = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  isDisabled(): boolean {
+      return this.disabled;
+  }
+
+  onClick() {
+    console.log("on click");
+    this.disabled = !this.disabled;
+  }
 }
