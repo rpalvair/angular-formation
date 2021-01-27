73bd0758a18ba4b4f05591f3d6b0f3e6174b9c43
ruddy palvair <r.palvair@gmail.com>
2021-01-27 09:31:09 +0100

    feat : Change color dynamically against for sale variable

--- monAgence/src/app/app.component.ts
+++ monAgence/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Mon Agence';
+
+  forSale = true;
+
+  getForSaleValue() {
+    return this.forSale ? "green" : "red";
+  }
 }
