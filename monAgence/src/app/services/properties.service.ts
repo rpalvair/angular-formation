d0839e118fd841fd6c405a952579934884df3e10
ruddy palvair <r.palvair@gmail.com>
2021-01-28 08:41:03 +0100

    feat : Use promise in service

--- monAgence/src/app/services/properties.service.ts
+++ monAgence/src/app/services/properties.service.ts
@@ -6,4 +6,31 @@ import { Injectable } from '@angular/core';
 export class PropertiesService {
 
   constructor() { }
+
+
+  private properties = [
+    {
+      'title': 'Ma superbe maison',
+      'category': 'Maison',
+      'forSale': false
+    },
+    {
+      'title': 'Petit appartement',
+      'category': 'Appartement',
+      'forSale': true
+    },
+    {
+      'title': 'Belle villa',
+      'category': 'Maison',
+      'forSale': false
+    }
+  ];
+
+  getProperties(): Promise<any[]> {
+    return new Promise(
+      (resolve, reject) => {
+        resolve(this.properties);
+      }
+    );
+  }
 }
