ccbc94d5a0750bac076e6896732cca9618fda80a
ruddy palvair <r.palvair@gmail.com>
2021-02-01 15:30:15 +0100

    feat : Import firebase in main component

--- monAgence/src/app/app.component.ts
+++ monAgence/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Mon Agence';
 
- 
+
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyBcdOejAx73vPQxgFVe5rUZric9wklEsP8",
+      authDomain: "monagence-15ac2.firebaseapp.com",
+      projectId: "monagence-15ac2",
+      storageBucket: "monagence-15ac2.appspot.com",
+      messagingSenderId: "400618735910",
+      appId: "1:400618735910:web:0f39b4ff994e30f2ec59e7"
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
 }
