678a6def8de4b0b68fa97fd11f0f125a1ee53a2a
ruddy palvair <r.palvair@gmail.com>
2021-02-02 10:36:01 +0100

    feat: Create signin form

--- monAgence/src/app/authentication/signin/signin.component.ts
+++ monAgence/src/app/authentication/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  signinForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initSigninForm()
   }
 
+
+  initSigninForm() {
+    this.signinForm = this.formBuilder.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]]
+      }
+    )
+  }
+
+  onSubmit() {
+    console.log("form", this.signinForm.value)
+  }
 }
