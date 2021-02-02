ce321706c55d1d4fcc85e866734a252fcf9c7d7d
ruddy palvair <r.palvair@gmail.com>
2021-02-02 18:46:51 +0100

    chore: Firebase configuration

--- monAgence/src/app/app.component.ts
+++ monAgence/src/app/app.component.ts
@@ -13,11 +13,12 @@ export class AppComponent {
   constructor() {
     const firebaseConfig = {
       apiKey: "",
-      authDomain: "monagence-15ac2.firebaseapp.com",
-      projectId: "monagence-15ac2",
-      storageBucket: "monagence-15ac2.appspot.com",
-      messagingSenderId: "400618735910",
-      appId: "1:400618735910:web:0f39b4ff994e30f2ec59e7"
+      authDomain: "monagence-fa8b4.firebaseapp.com",
+      databaseURL: "https://monagence-fa8b4-default-rtdb.firebaseio.com",
+      projectId: "monagence-fa8b4",
+      storageBucket: "monagence-fa8b4.appspot.com",
+      messagingSenderId: "707981701536",
+      appId: "1:707981701536:web:9c6d5395b001b605b3b4c5"
     };
     firebase.initializeApp(firebaseConfig);
   }
