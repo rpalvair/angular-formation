532f37cede940337d06a66d68b266d1e2975b7dd
ruddy palvair <r.palvair@gmail.com>
2021-02-02 10:45:34 +0100

    feat : Signup with firebase

--- monAgence/src/app/services/authentication.service.ts
+++ monAgence/src/app/services/authentication.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,21 @@ import { Injectable } from '@angular/core';
 export class AuthenticationService {
 
   constructor() { }
+
+  signUpUser(email: string, password: string): Promise<void> {
+    return new Promise<void>(
+      (resolve, reject) => {
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(
+          () => {
+            console.log("connecté")
+            resolve()
+          }
+        ).catch(
+          (error) => {
+            reject(error)
+          }
+        )
+      }
+    )
+  }
 }
